553fb53b888825abb979c23d7ee318f300a0ee8e
rachelmathilda <rachelmathilda10@gmail.com>
2026-05-05 11:26:19 +0700

    feat: initialize spring boot app

---
+++ src/main/java/com/bankflow/bankflow_api/BankflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.bankflow.bankflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankflowApiApplication.class, args);
+	}
+
+}
